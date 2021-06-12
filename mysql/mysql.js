a74394562f1fb3b858f687b0c226c8889f167c7a
Seba <sebastian.infante.dg@gmail.com>
2021-06-12 00:16:00 -0400

    add client's table and form CRUD

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -1,13 +1,25 @@
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-    host:'localhost',
-    user: 'root',
-    password: '',
-    database: 'zyrapp'
-})
+function show() {
+  const mysql = require("mysql");
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "zyrapp",
+  });
 
-connection.connect((err)=>{
-    if(err) throw err
-    console.log('La conexion funciona');
-})
-connection.end()
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("La conexion funciona");
+  });
+
+  connection.query("select * from clients", (err, rows) => {
+    if (err) throw err;
+    var clients = rows;
+    console.log(clients);
+  });
+  connection.end();
+
+//   var objetivo = document.getElementById("texto_nav1");
+//   objetivo.innerHTML = clients;
+}
+show()
